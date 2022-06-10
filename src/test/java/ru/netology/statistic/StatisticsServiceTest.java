a37fc03cf02871ac5323c9e99ca32b8ef1b1f4bf
Roman Medvedev <romankazan88@gmail.com>
2022-06-10 20:04:49 +0300

    100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,16 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInTheEnd() {
+       StatisticsService service = new StatisticsService();
+
+     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12,99};
+    long expected = 99;
+
+      long actual = service.findMax(incomesInBillions);
+
+       assertEquals(expected, actual);
+   }
 }
